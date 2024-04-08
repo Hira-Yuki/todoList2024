77833204592b1910eaa4c44a25d5af41c08e66e1
Hira-Yuki <cjsdlf44@gmail.com>
2024-04-08 16:14:43 +0900

    프로젝트 생성, 마크업 작업

---
+++ src/ListItem/TodoItem.tsx
@@ -0,0 +1,21 @@
+import styles from './TodoItem.module.css'
+import { BsCheckCircle } from 'react-icons/bs'
+import { IoIosRemoveCircleOutline } from 'react-icons/io'
+
+
+interface TodoItemProps {
+  text: string
+}
+const TodoItem = (props: TodoItemProps) => {
+  const isChecked = false
+
+  return (
+    <li className={styles.container}>
+      <BsCheckCircle className={[styles.checkIcon, `${isChecked ? styles.checkedCircleIcon : styles.unCheckedCircleIcon}`].join(" ")} />
+      <span>{props.text}</span>
+      <IoIosRemoveCircleOutline className={styles.removeIcon} />
+    </li>
+  )
+}
+
+export default TodoItem
